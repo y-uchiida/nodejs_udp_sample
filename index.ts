7b2c72ca18784c04fd87a256f317da401a9f22bf
yugo uchiida <y.uchiida@nirneege.com>
2023-01-14 19:07:01 +0900

    パケットを受信した時刻をファイル名にして内容を出力する実装を追加

--- index.ts
+++ index.ts
@@ -1,16 +1,20 @@
 import dgram from "node:dgram";
 import { appendFile } from "fs";
+import { formatDate } from "./formatDate";
 
 const server = dgram.createSocket("udp4");
 
 server.on("error", (err) => {
   console.error(`server error:\n${err.stack}`);
+  console.error(err);
   server.close();
 });
 
 server.on("message", async (msg, rinfo) => {
   // 受信した内容をファイルに書き込む
-  await appendFile("udp_reserved", msg, () => { });
+  const now = new Date();
+  const fileName = formatDate(now, "yyyy_MM_dd_HH:mm:ss:SSS");
+  await appendFile(`${__dirname}/${fileName}`, msg, () => { });
   console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
 });
 
@@ -20,4 +24,3 @@ server.on("listening", () => {
 });
 
 server.bind(41234, "127.0.0.1");
-// Prints: server listening 127.0.0.1:41234
